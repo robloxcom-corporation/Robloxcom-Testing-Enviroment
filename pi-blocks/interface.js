863bbe2da9c4b6ac67a595640943a17584b0690b
Blizard_Wizard <BlizardWizard@users.noreply.github.com>
2019-03-18 23:15:58 -0700

    finished cols equations and added accuracy value and slider

--- pi-blocks/interface.js
+++ pi-blocks/interface.js
@@ -1,12 +1,16 @@
 
-var slider = document.getElementById("slider");
-var display = document.getElementById("display");
+var pow_slider = document.getElementById("pow_slider")
+var pow_slider_display = document.getElementById("pow_slider_display");
+pow_slider.onchange = function() {
+  pow_slider_display.innerHTML = pow_slider.value;
+};
 
-slider.onchange = function() {
 
-  display.innerHTML = slider.value
-
-}
+var acc_slider = document.getElementById("acc_slider");
+var acc_slider_display = document.getElementById("acc_slider_display");
+acc_slider.onchange = function() {
+  acc_slider_display.innerHTML = acc_slider.value;
+};
 
 
 var start = document.getElementById("start");
@@ -28,5 +32,11 @@ var reset = document.getElementById("reset");
 reset.onclick = function() {
   looping = false;
   context.clearRect(0, 0, canvas.width, canvas.height);
-  setup(document.getElementById("slider").value);
-}
+  setup(document.getElementById("pow_slider_display").value, document.getElementById("acc_slider_display").value);
+};
+
+
+function updateCols(value) {
+  var cols = document.getElementById("cols");
+  cols.innerHTML = value;
+};
--- pi-blocks/main.js
+++ pi-blocks/main.js
@@ -1,76 +1,95 @@
 
 var blocks = [];
 var looping = false;
+var block1;
+var block2;
+var times = 100000;
+var cols = 0;
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 var slider = document.getElementById("slider");
 
-var block1;
-var block2;
+function setup(power, accuracy) {
+  cols = 0;
+  times = accuracy;
+  console.log(power);
+  console.log(times);
 
-function setup(power) {
-console.log(power)
-block1 = new Component(canvas.width * 5/6, canvas.height * 3/4, 40, 40, "rect");
-block1.color = "#33aaff";
-block1.vel = -1;
-block1.mass = Math.pow(100, power);
+  block1 = new Component(canvas.width * 5/6, canvas.height * 3/4, 40, 40, "rect");
+  block1.color = "#33aaff";
+  block1.vel = -1 / times;
+  block1.mass = Math.pow(10, power);
 
-block2 = new Component(canvas.width * 1/3, canvas.height * 3/4 + 20, 20, 20, "rect");
-block2.color = "#33aaff";
-block2.vel = 0;
-block2.mass = 1;
+  block2 = new Component(canvas.width * 1/3, canvas.height * 3/4 + 20, 20, 20, "rect");
+  block2.color = "#33aaff";
+  block2.vel = 0 / times;
+  block2.mass = 1;
 
-
-Component.prototype.step = function() {
-  this.x = this.x + this.vel;
-};
-
-Component.prototype.intersects = function(block) {
-  if (this.x <= block.x + block.width) {
-    return true;
-  } else {
-    return false
+  Component.prototype.step = function() {
+    this.x = this.x + this.vel;
   };
-};
 
-Component.prototype.hitsWall = function() {
-  if (this.x <= 0) {
-    return true
-  } else {
-    return false;
+  Component.prototype.intersects = function(block) {
+    if (this.x <= block.x + block.width) {
+      return true;
+    } else {
+      return false
+    };
   };
+
+  Component.prototype.hitsWall = function() {
+    if (this.x <= 0) {
+      return true
+    } else {
+      return false;
+    };
+  };
+
+  Component.prototype.collide = function(block) {
+    var sum_mass = this.mass + block.mass;
+    var vel_ = (this.mass - block.mass) / sum_mass * this.vel;
+    vel_ += (2 * block.mass / sum_mass) * block.vel;
+    return vel_;
+  };
+
+  block1.draw();
+  block2.draw();
+
 };
 
-Component.prototype.collide = function(block) {
-  var _this = this;
-  this.vel = ((_this.mass - block.mass) / (_this.mass + block.mass) * _this.vel)
-    + (2 * block.mass / (_this.mass + block.mass) * block.mass);
-
-  block.vel = (2 * _this.mass / (_this.mass + block.mass) * _this.vel)
-    + ((block.mass - _this.mass) / (_this.mass + block.mass) * block.vel);
-};
-
-block1.draw();
-block2.draw();
-
-};
-
-setup(2);
+setup(0, 1);
 
 function animate( timestamp ) {
 
   block1.clear();
-  block1.step();
-
   block2.clear();
-  block2.step();
 
-  if (block1.intersects(block2)) {
-    console.log("hello")
-    block1.collide(block2);
+  for (var i = 0; i < times; i++) {
 
-  };
+    if (block1.intersects(block2)) {
+      var vel_1 = block1.collide(block2);
+      var vel_2 = block2.collide(block1);
+
+      block1.vel = vel_1;
+      block2.vel = vel_2;
+
+      cols++;
+      updateCols(cols);
+
+    };
+
+    if (block2.x <= 0) {
+      block2.vel = block2.vel *-1;
+
+      cols++;
+      updateCols(cols);
+    };
+
+    block1.step();
+    block2.step();
+
+};
 
   block1.draw();
   block2.draw();
